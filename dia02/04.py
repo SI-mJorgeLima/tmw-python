4c7c01d1bbc371ccdcfef396ad02fb40f9ee9ea4
Jorge <mjorgelima98@gmail.com>
2026-08-18 23:53:47 -0300

    anotações

--- dia02/04.py
+++ dia02/04.py
@@ -1,4 +1,8 @@
 from math import sqrt
 
 numero = int(input('qual numero gostaria de calcular a raiz? '))
+
+#raiz = numero**0.5
+#raiz = round(numero, 2)
+
 print('A raiz de', numero, "é: ", sqrt(numero))
